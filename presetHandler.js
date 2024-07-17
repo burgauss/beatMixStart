4d2449982784ba3f81e11492760fe6d388e47182
Juan Burgos <burgos.135@gmail.com>
2024-07-17 13:37:47 -0600

    implementing functions in presetHandler and scrip.js

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,28 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (request, index, newPressetArray = null) => {
+    if (index >= 0 || index < presets.lenght)
+    {
+    
+        if (request === 'GET')
+            {
+                return [200, presets[index]];
+            } else if (request === 'PUT')
+            {
+                return [200, newPressetArray[index]];
+            } else
+            {
+                // Bad Request
+                return [400, ];
+            }
+    } else
+    {
+        // invalid index
+        return [404];
+    }
+
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,53 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = new Array(16).fill(false);
+let snares = new Array(16).fill(false);
+let hiHats = new Array(16).fill(false);
+let rideCymbals = new Array(16).fill(false);
 
+const drumTypes = ['kicks', 'snares', 'hiHats', 'rideCymbals'];
+
+function toggleDrum(s_drumType, index){
+    if (drumTypes.includes(s_drumType)){
+        getArrayVar(s_drumType)[index] = !getArrayVar(s_drumType)[index];
+    } else{
+        return null;
+    }
+}
+
+//function takes a string and it returns the variable associated with it
+function getArrayVar(s_drumType)
+{
+    switch (s_drumType) {
+        case 'kicks':
+            return kicks;
+        case 'snares':
+            return snares;
+        case 'hiHats':
+            return hiHats;
+        case 'rideCymbals':
+            return rideCymbals;
+        default:
+            return null;
+    }
+}
+
+function clear(s_drumType){
+    if (drumTypes.includes(s_drumType))
+    {
+        getArrayVar(s_drumType).fill(false)
+    } else{
+        console.log("invalid drum type name");
+    }
+}
+
+function invert(s_drumType){
+    if (drumTypes.includes(s_drumType))
+    {
+        for (let i= 0; i < getArrayVar(s_drumType).length; i++)
+        {
+            getArrayVar(s_drumType)[i]= !getArrayVar(s_drumType)[i];
+        }
+    } else{
+        console.log("not possible to invert");
+    }
+}
